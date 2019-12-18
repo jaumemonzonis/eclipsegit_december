482b409d64ae537660436c81c17792c5528a4400
jaume monzonis <jaume_monzonis@hotmail.com>
2019-12-18 16:34:26 +0100

    commit initial

---
+++ src/main/java/com/jaume/pruebagithub/App.java
@@ -0,0 +1,13 @@
+package com.jaume.pruebagithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worlwwwwd!" );
+    }
+}
