d220c5944328882a72cea5491e18caf6064900e3
jaume monzonis <jaume_monzonis@hotmail.com>
2019-12-18 16:37:50 +0100

    commit inicial 2

--- src/main/java/com/jaume/pruebagithub/App.java
+++ src/main/java/com/jaume/pruebagithub/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worlwwwwd!" );
+        System.out.println( "Hello Worrrlwwwwd!" );
     }
 }
